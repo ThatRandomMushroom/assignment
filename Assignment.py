9cfc5a92271706709f7dc7236fbcddd190396418
ThatRandomMushroom <jrgshaw3121@gmail.com>
2019-02-18 16:10:44 +0000

    Assignment
    Signed-off-by: ThatRandomMushroom <jrgshaw3121@gmail.com>

---
+++ Assignment.py
@@ -0,0 +1,20 @@
+import sys
+from dictionary import alpha
+running = True
+def main():
+    ascinput = str(input("Write your text: \n>>> "))
+    if ascinput == "":
+        sys.exit()
+    try:
+        for row in range(len(alpha["a"])):
+            for word in ascinput:
+                print((alpha[word][row]), end=" ")
+            print()
+    except KeyError:
+        print("You have entered a symbol that is not in the dictionary yet")
+
+if __name__ == "__main__":
+    main()
+
+while True:
+    main()
